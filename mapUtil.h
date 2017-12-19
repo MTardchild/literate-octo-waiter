4899eba7fc8e60e699306349837fcbca3e39743e
Richard Tscharntke <superritschy@gmx.net>
2017-12-19 12:10:55 +0100

    fixed pathfinding

--- mapUtil.h
+++ mapUtil.h
@@ -6,7 +6,7 @@
 #define OCTA_MAP_UTIL_H
 
 #include "arenas.h"
-#include "octa_util.h"
+#include "octaUtil.h"
 
 #define direction_t byte
 #define no_dir 0
@@ -18,7 +18,7 @@
 
 #define STARTING_DIRECTION s
 
-#define start 4
+#define starting_square 4
 #define border 8
 #define empty 0
 #define table_1 1
@@ -36,32 +36,90 @@ byte next_to_goal(byte row, byte col, byte goal);
 #define map_val(row, col) mu_parsed_map[(col) + (row) * 14]
 
 
-#define get_top_4(num) (((num) & 0xf0) >> 4)
-#define get_row get_top_4
+#define get_row(num) (((num) & 0xf0) >> 4)
+//#define get_row get_top_4
 
-#define get_bot_4(num) ((num) & 0x0f)
-#define get_col get_bot_4
+#define get_col(num) ((num) & 0x0f)
+//#define get_col get_bot_4
 
-#define get_top_3(num) (((num) & 0xe0) >> 5)
-#define get_dir get_top_3
+#define get_dir(num) (((num) & 0xe0) >> 5)
+//#define get_dir get_top_3
 
-#define get_bot_5(num) ((num) & 0x1f)
-#define get_turns get_bot_5
+#define get_turns(num) ((num) & 0x1f)
+//#define get_turns get_bot_5
 
-#define set_top_4(num, val) (((num) & 0x0f) | ((val) << 4))
-#define set_row set_top_4
+#define set_row(num, val) (((num) & 0x0f) | ((val) << 4))
+//#define set_row set_top_4
 
-#define set_bot_4(num, val) (((num) & 0xf0) | (val))
-#define set_col set_bot_4
+#define set_col(num, val) (((num) & 0xf0) | (val))
+//#define set_col set_bot_4
 
-#define set_top_3(num, val) (((num) & 0x1f) | ((val) << 5))
-#define set_dir set_top_3
+#define set_dir(num, val) (((num) & 0x1f) | ((val) << 5))
+//#define set_dir set_top_3
 
-#define set_bot_5(num, val) (((num) & 0xe0) | (val))
-#define set_turns set_bot_5
+#define set_turns(num, val) (((num) & 0xe0) | (val))
+//#define set_turns set_bot_5
 
 
-byte mu_parsed_map[ARENA_SIZE];
+byte mu_parsed_map[ARENA_SIZE] = {0};
+
+
+byte adjust_row(byte row, direction_t dir){
+    if (dir == n)
+        return row - 1;
+    if (dir == s)
+        return row + 1;
+    return row;
+}
+
+
+byte adjust_col(byte col, direction_t dir){
+    if (dir == w)
+        return col - 1;
+    if (dir == e)
+        return col + 1;
+    return col;
+}
+
+
+bool is_border(byte row, byte col, direction_t dir){
+    row = adjust_row(row, dir);
+    col = adjust_col(col, dir);
+    if (row == 0 || row == 13 || col == 0 || col == 13)
+        return true;
+    return false;
+}
+
+
+bool visited(byte row, byte col, direction_t dir){
+    row = adjust_row(row, dir);
+    col = adjust_col(col, dir);
+    if (map_val(row, col) != 0)
+        return true;
+    return false;
+}
+
+
+bool is_table(byte row, byte col, direction_t dir){
+    row = adjust_row(row, dir);
+    col = adjust_col(col, dir);
+    byte val = map_val(row, col);
+    if (val == table_1 || val == table_2 || val == table_3)
+        return true;
+    return false;
+}
+
+byte next_to_goal(byte row, byte col, byte goal){
+    direction_t dir;
+    for (dir = n; dir <=w; dir++){
+        byte r = adjust_row(row, dir);
+        byte c = adjust_col(col, dir);
+        if (map_val (r, c) == goal)
+            return dir;
+    }
+    return no_dir;
+}
+
 
 
 
--- pathfinding.h
+++ pathfinding.h
@@ -5,7 +5,7 @@
 #ifndef OCTA_PATHFINDING_H
 #define OCTA_PATHFINDING_H
 
-#include "map_util.h"
+#include "mapUtil.h"
 #include "parser.h"
 
 #define start_of_search 5
@@ -13,7 +13,6 @@
 #define QUEUE_SIZE 36
 #define MAX_PATH_SIZE 100
 
-byte pf_path[MAX_PATH_SIZE];
 
 /*
  * Takes mu_parsed_map and finds the shortest path to goal with
@@ -23,6 +22,134 @@ byte pf_path[MAX_PATH_SIZE];
  * The resulting path is written to pf_path.
  */
 void find_path(byte row, byte col, byte goal);
+
+
+//private implementation:
+
+
+byte pf_path[MAX_PATH_SIZE];
+
+
+void trace_back_path(byte row, byte col, byte prev_dir) {
+    direction_t dir;
+    byte val, i = 0;
+    for (i = 0; i<MAX_PATH_SIZE; i++)
+        pf_path[i] = 0;
+    i = 0;
+    while (true){
+        val = map_val(row, col);
+        if (get_dir(val) == start_of_search) {
+            if (prev_dir != STARTING_DIRECTION)
+                pf_path[i] = prev_dir;
+            break;
+        }
+        dir = opposite_dir(get_dir(val));
+        if (prev_dir != dir)
+            pf_path[i++] = prev_dir;
+        pf_path[i++] = dir;
+        prev_dir = dir;
+        row = adjust_row(row, opposite_dir(dir));
+        col = adjust_col(col, opposite_dir(dir));
+    }
+}
+
+void turn_path(){
+    byte i, tail, head;
+    for (i = 0; i<MAX_PATH_SIZE; i++) {
+        if (pf_path[i] == 0)
+            break;
+    }
+    if (i == 0)
+        return;
+    tail = i - 1;
+    head = 0;
+    while (tail > head) {
+        byte temp = pf_path[head];
+        pf_path[head++] = pf_path[tail];
+        pf_path[tail--] = temp;
+    }
+}
+
+
+void goal_found(byte row, byte col, direction_t dir){
+    trace_back_path(row, col, dir);
+    turn_path();
+    parse();
+}
+
+
+void less_turns(byte r, byte c, byte turns, direction_t prev_dir, direction_t dir) {
+    byte prev_turns = get_turns(map_val(r, c));
+    byte val;
+    if (prev_dir == dir && turns < prev_turns){
+        val = set_dir(turns, opposite_dir(dir));
+        map_val(r, c) = val;
+    }else if (turns + 1 < prev_turns) {
+        val = set_dir(turns + 1, opposite_dir(dir));
+        map_val(r, c) = val;
+    }
+}
+
+
+void b_first_search(byte row, byte col, byte goal) {
+	byte queue[QUEUE_SIZE];
+	byte i;
+	for (i=0;i<QUEUE_SIZE;i++)
+		queue[i] = 0;
+    byte head, tail;
+    queue[0] = set_row(col, row);
+    head = 0;
+    tail = 1;
+	byte test;
+
+    map_val(row, col) = set_dir(map_val(row, col), start_of_search);
+	
+    while (head != tail) {
+		row = get_row(queue[head]);
+		col = get_col(queue[head]);
+        
+		head = (head + 1) % QUEUE_SIZE;
+	
+        direction_t dir_to_goal = next_to_goal(row, col, goal);
+
+        if (dir_to_goal) {
+            goal_found(row, col, dir_to_goal);
+            break;
+        }
+        direction_t prev_dir = opposite_dir(get_dir(map_val(row, col)));
+        byte turns = get_turns(map_val(row, col));
+
+        direction_t dir;
+		
+        for (dir = n; dir<=w; dir++) {
+			
+            byte r = adjust_row(row, dir);
+            byte c = adjust_col(col, dir);
+            byte val = map_val(r, c);
+			
+            if (val == empty){
+				
+                turns = turns + ((dir != prev_dir) ? 1 : 0);
+                map_val(r, c) = set_dir(turns, opposite_dir(dir));
+                queue[tail] = set_row(c, r);
+                tail = (tail + 1) % QUEUE_SIZE;
+            }else if (get_dir(val) != empty && get_dir(val) != start_of_search) {
+				
+                less_turns(r, c, turns, prev_dir, dir);
+            }
+			
+        }
+    }
+	
+} 
+
+void find_path(byte row, byte col, byte goal){
+	
+    if (row < 14 && row >= 0 && col < 14 && col >= 0)
+        b_first_search(row, col, goal);
+    //else
+    //    exit(1);
+}
 
 
 #endif //OCTA_PATHFINDING_H
