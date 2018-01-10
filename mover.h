3fff1a619d3509faa4892ed1c7bdf6b2f1a053cd
Marius Tuschl <marius.tuschl@st.oth-regensburg.de>
2018-01-10 18:01:50 +0100

    Implemented most features of OPS.

--- mover.h
+++ mover.h
@@ -1,34 +1,27 @@
 #ifndef OCTA_MOVER_H
 #define OCTA_MOVER_H
 
-/* 
- *  Moves the path calculated by pathfinder.
- *  Default smoothness and power values.
- */
-void movePath();
+
+#define TURN_EPSILON_ROUGH 5
+#define TURN_EPSILON_FINE 0.4
+#define DRIVE_EPSILON_ROUGH ??
+#define DRIVE_EPSILON_FINE ??
+
+#define GO_QUICK 35
+#define GO_SLOW 15
+#define TURN_QUICK 15
+#define TURN_SLOW 5
+
 
 /*
- *  Drives in currently facing direction. Default power value.
- *
- *  distance: Determines how far the robot should be driving (1 - 12)
- *  smooth: Determines if the motion should be smooth (true/false).
- */
-void goStraight(byte distance, byte smooth){
-
-
+*  Converts ops style degrees (0 - 359) to path style directions (1 - 4) 
+*
+*  direction: direction to be converted
+*/
+int convertFromDeg(byte direction){
+	return (direction + 45) / 90;
 }
 
-/*
- *  Turns the robot so that it faces the given direction. Default power value.
- *
- *
- *  direction: Determines the direction the robot should face afterwards. (1 - 4)
- *  smooth: Determines if the motion should be smooth (true/false).
- */
-void turn(byte direction, byte smooth){
-	
-
-}
 /*
 *  Converts path style directions (1 - 4) to ops style degrees (0 - 359)
 *
@@ -39,32 +32,145 @@ int convertToDeg(byte direction){
 	return direction * 90;
 }
 
-// THIS DOES NOT WORK
-bool isFacingDirection(short direction) {
-	direction = convertToDeg(direction)
-    return ops_direction >= ((direction - EPSILON_DIRECTION) % 360)
-        && ops_direction <= ((direction + EPSILON_DIRECTION) % 360) ;
+#define min(x, y) ((x)<(y) ? (x) : (y))
+#define max(x, y) ((x)>(y) ? (x) : (y))
+
+bool isFacingDirection(byte direction, float epsilon) {
+	x = min(direction, ops_dir);
+	y = max(direction, ops_dir)
+	x += (y - x > 180) ? 360 : 0;
+	return abs(y-x) < epsilon;
 }
+
+bool turnLeft(short direction){
+	direction += (abs(y - x) > 180) ? 360 : 0;
+	return (ops_dir - direction) > 0;
+}
+
 
 /*
  *  Returns: If the path has been finished
  */
 #define isOver(currentStep) (currentStep != MAX_PATH_SIZE && pf_path[currentStep] != 0)
 
+/* 
+ *  Moves the path calculated by pathfinder.
+ *  Default smoothness and power values.
+ */
+void movePath();
+
+/*calculats the target square in the current direction
+*
+*/
+byte getTargetSquare(byte distance){
+	byte direction = convertFromDeg(ops_dir);
+	switch(direction)
+	case north:
+		return ops_y - distance;
+	case east:
+		return ops_x + distance;
+	case south:
+		return ops_y + distance;
+	case west:	
+		return ops_x - distance;
+	default:
+		//dunno
+		return 0;
+}
+
+/*
+*   chechs if target square in the current driving direction has been reached
+*/
+bool reachedTarget(byte target, float epsilon){
+	byte direction = convertFromDeg(ops_dir);
+	byte current_in_dir_value;
+	if (direction == north || direction == south)
+		current_in_dir_value = ops_y;
+	else if (direction == north || direction == south)
+		current_in_dir_value = ops_x;
+	if (abs(current_in_dir_value - epsilon)<epsilon)
+		return true;
+	return false;	
+}
+
+/*
+ *  Drives in currently facing direction. Default power value.
+ *
+ *  distance: Determines how far the robot should be driving (1 - 12)
+ *  smooth: Determines if the motion should be smooth (true/false).
+ */
+void goStraight(byte distance, byte smooth){
+	byte targetSquare = getTargetSquare(distance); 
+	if(smooth){
+		while(!reachedTarget(targetSquare, DRIVE_EPSILON_ROUGH)){
+			OnFwd(OUT_A, pidController(OUT_A, GO_QUICK, rpmA);
+			OnFwd(OUT_B, pidController(OUT_B, GO_QUICK, rpmB);
+		}
+		while(!reachedTarget(targetSquare, DRIVE_EPSILON_FINE)){
+			OnFwd(OUT_A, pidController(OUT_A, GO_SLOW, rpmA);
+			OnFwd(OUT_B, pidController(OUT_B, GO_SLOW, rpmB);
+		}
+	}else{
+		while (!reachedTarget(targetSquare, DRIVE_EPSILON_FINE)){
+			OnFwd(OUT_A, pidController(OUT_A, GO_QUICK, rpmA);
+			OnFwd(OUT_B, pidController(OUT_B, GO_QUICK, rpmB);
+		}
+	}
+	Off(OUT_AB);
+}
+
+#define turnLeft  -1
+#define turnRight  1
+
+/*
+ *  Turns the robot so that it faces the given direction. Default power value.
+ *
+ *
+ *  direction: Determines the direction the robot should turn (1 - 4)
+ *  smooth: Determines if the motion should be smooth (true/false)
+ *  turnDir: turnLeft / turnRight
+ */
+ 
+ void turn(byte direction, byte smooth){
+	direction = convertToDeg(direction)
+	if(turnLeft(direction))
+		goTurn(direction, smooth, turnLeft);
+	else
+		goTurn(direction, smooth, turnRight);
+	
+	if(smooth){
+		while(!isFacingDirection(direction, TURN_EPSILON_ROUGH)){
+			OnFwd(OUT_A, pidController(OUT_A, TURN_QUICK, rpmA * turnDir);
+			OnFwd(OUT_B, pidController(OUT_B, TURN_QUICK, rpmB * turnDir * -1);
+		}
+		while(!isFacingDirection(direction, TURN_EPSILON_FINE)){
+			OnFwd(OUT_A, pidController(OUT_A, TURN_SLOW, rpmA * turnDir);
+			OnFwd(OUT_B, pidController(OUT_B, TURN_SLOW, rpmB * turnDir * -1);
+		}
+	}else{
+		while (!isFacingDirection(direction, TURN_EPSILON_FINE)){
+			OnFwd(OUT_A, pidController(OUT_A, TURN_QUICK, rpmA * turnDir);
+			OnFwd(OUT_B, pidController(OUT_B, TURN_QUICK, rpmB * turnDir * -1);
+		}
+	}
+	Off(OUT_AB);
+}
+
  
 void movePath(bool smooth) {
 	int currentStep = 0;
     while (!isOver(currentStep) {
         consecutiveSameDirections = 1;
-        if (isFacingDirection(pf_path[currentStep]) {
+        if (isFacingDirection(pf_path[currentStep], TURN_EPSILON_ROUGH) {
             ++currentStep;
-            while(isFacingDirection(pf_path[currentStep]) && !isOver(currentStep){
+            while(isFacingDirection(pf_path[currentStep], TURN_EPSILON_ROUGH) && !isOver(currentStep){
                 ++consecutiveSameDirections;
                 ++currentStep;
             }
-            go_straight(consecutiveSameDirections, smooth)
+            goStraight(consecutiveSameDirections, smooth)
         } else {
-            turn(pf_path[currentStep], smooth)
+            turn(pf_path[currentStep], smooth);
+			++currentStep;
         }
     }
 }
--- octaPositioningSystem.h
+++ octaPositioningSystem.h
@@ -6,28 +6,29 @@
 /*
  *	x position (square wise)
  */
-byte xSquare;
+byte ops_xSquare;
 
 /*
  *	y position (square wise)
  */
-byte ySquare;
+byte ops_ySquare;
 
 /*
  *	x position inside square (0 - 255)
  */
-byte x;
+byte ops_x = 127;
 
 /*
  *	y position inside square (0 - 255)
  */
-byte y;
+byte ops_y = 127;
 
 /*
  *	The direction waiter is facing in degrees (0-359)
  *  0 = North
+ *  	The robot always starts facing south.
  */
-int direction;
+float ops_dir = 180;
 
 /*
  *	Cm equivalent of a square point.
@@ -40,15 +41,82 @@ const float squarePointDistance = 0.09803922;
 const float rotationDistance = 13.67;
 
 /*
+ *	Represents 360/Pi.
+ */
+const float turnConst = 114.591559026;
+
+/*
+ *	Distance between the middle of the wheels in cm.
+ */
+const float wheelBase = 10.2;
+
+/*
  *	Calculates the current position and writes result into global variables. 
  *	This method assumes that it gets called every OPS_CALC_FREQUENCY ms.
  */
 task calculatePosition() {
 	while(1) {
+		float dpmA = rpmToDpm(OUT_A); 
+		float dpmB = rpmToDpm(OUT_B);
 		Wait(OPS_CALC_FREQUENCY);
-		static float dpmA = rpmToDpm(OUT_A); 
-		static float dpmB = rpmToDpm(OUT_B);
+		dpmToAngle(dpmA, dpmB);	
+		dpmToDistance(dpmA, dpmB);
 	}
+}
+
+/*
+ *	Adds given distance to x and y coordinates.
+ */
+void addDistance(int distanceX, int distanceY) {
+	ops_x += distanceX;
+	ops_y += distanceY;
+}
+
+/*
+ *	Calculates the new coordinates.
+ */
+void dpmToDistance(float dpmA, float dpmB) {
+	float average = (dpmA + dpmB) / 2;
+	float distance = average * OPS_CALC_FREQUENCY / 1000;
+	// TODO: check for sign
+	float ratioX = getRatioX();
+	float ratioY = 1 - ratioX;
+	addDistance(average * ratioX, average * ratioY);
+}
+
+/*
+ *	Calculates how much the robot drove into x direction.
+ *	Relative to y.
+ *	Examples:
+ *	if ops_dir = 45 the ratio will be 0.5 for x and y.
+ *	if ops_dir = 90 the ratio will be 1 for x and 0 for y.
+ */
+float getRatioX() {
+	int ratio = ops_dir % 180;
+	if (ratio > 90) ratio -= 90;
+	float ratioX = ratio / 90;
+	
+	return ratioX;
+}
+
+/*
+ *	Adds given value to direction variable.
+ *	Takes care of overflow.
+ */
+void addDirection(float value) {
+	ops_dir += value;
+	ops_dir -= ops_dir > 359 ? 360 : 0; 	
+}
+
+/*
+ *	Calculates the new coordinate given the dpm.
+ */
+void dpmToAngle(float dpmA, float dpmB) {
+	float distanceAxisA = dpmA * OPS_CALC_FREQUENCY / 1000;
+	float distanceAxisB = dpmB * OPS_CALC_FREQUENCY / 1000;
+	float distanceDiff = distanceAxisA - distanceAxisB;
+	float directionOffset = (distanceDiff/wheelBase) * turnConst;	
+	addDirection(directionOffset);
 }
 
 /*
